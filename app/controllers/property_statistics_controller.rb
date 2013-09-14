7ae581940effc6eeeb8146638847c9d35d227b3c
root <root@ubuntu.(none)>
2013-09-14 14:07:28 -0700

    fixed indentation

--- app/controllers/property_statistics_controller.rb
+++ app/controllers/property_statistics_controller.rb
@@ -1,11 +1,11 @@
 class PropertyStatisticsController < ApplicationController
   def new
-  	@property_statisitc =	PropertyStatistic.new
+    @property_statisitc = PropertyStatistic.new
   end
 
   def create
-  	@property_statisitc = PropertyStatistic.new(create_params)
-  	if @property_statisitc.save
+    @property_statisitc = PropertyStatistic.new(create_params)
+    if @property_statisitc.save
       render :create
     else
       render :error
@@ -15,7 +15,6 @@ class PropertyStatisticsController < ApplicationController
   private
 
   def create_params
-  	params.require(:property_statistic).permit!
+    params.require(:property_statistic).permit!
   end
-
 end
--- app/models/property_statistic.rb
+++ app/models/property_statistic.rb
@@ -1,18 +1,18 @@
 class PropertyStatistic < ActiveRecord::Base
-	validates :area, :selling_price, :purchasing_price, presence: true
-	validates_numericality_of	:area, :selling_price, :purchasing_price,greater_than: 0
+    validates :area, :selling_price, :purchasing_price, presence: true
+    validates_numericality_of   :area, :selling_price, :purchasing_price,greater_than: 0
 
 
-	def profit_percentage
-		# ROI = (cost of property - value of sale) / cost of property * 100
-		((selling_price - purchasing_price) * 100.0)/purchasing_price
-	rescue Exception => each 
-		"0"
-	end
+    def profit_percentage
+        # ROI = (cost of property - value of sale) / cost of property * 100
+        ((selling_price - purchasing_price) * 100.0)/purchasing_price
+        rescue Exception => each 
+            "0"
+    end
 
-	def profit_per_square
-		(selling_price - purchasing_price)/(area * 1.0)
-		rescue Exception => each 
-		"0"
-	end
+    def profit_per_square
+        (selling_price - purchasing_price)/(area * 1.0)
+        rescue Exception => each 
+            "0"
+    end
 end
